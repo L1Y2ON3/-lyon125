d112abba0500da86dd1e468e64883c8ce72e53bc
L1Y2ON3 <gregorymuhia@gmail.com>
2025-05-20 01:10:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,106 @@
-let modal = document.querySelector('.modal');
-let modalImg = document.getElementById("modalImage");
-let captionText = document.getElementById("caption");
-let closeBtn = document.querySelector(".close");
-document.querySelectorAll('.photo img').forEach(img => {
-    img.addEventListener('click', function () {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerText = this.alt;
-    });
-});
-closeBtn.onclick = function () {
-    modal.style.display = "none";
-};
-window.onclick = function (EVENT) {
-    if (event.target === modal) {
-        modal.style.display = "none";
+function section1(cc) {
+    let term1 = cc / 4;
+    let term2 = -2 * cc;
+    let term3 = -1;
+  
+    return term1 + term2 + term3;
+  }
+  
+  function section2(yy) {
+    let term1 = yy * 5;
+    return term1 / 4;
+  }
+  
+  function section3(mm) {
+    let term1 = 26;
+    let term2 = mm + 1;
+    let term3 = term1 * term2;
+    return term3 / 10;
+  }
+  
+  function submitForm() {
+    console.log("form submited");
+    let genderInput = document.getElementById("gender_input").value;
+    let dob = document.getElementById("date_input").value;
+    let akan_name_el = document.getElementById("akan_name");
+  
+    if (!genderInput || !dob) {
+      return;
     }
-};
-document.querySelectorAll('.filter').forEach(filter => {
-    filter.addEventListener('click', function (e) {
-        e.preventDefault();
-        let category = this.getAttribute('data-category');
-        document.querySelectorAll('.photo').forEach(photo => {
-            let photoCategory = photo.getAttribute('data-category');
-            if (category === 'all' || photoCategory === category) {
-                photo.style.display = 'block';
-            } else {
-                photo.style.display = 'none';
-            }
-        });
-    });
-});
+  
+    let CC = dob.slice(0, 2);
+    console.log("CC is", CC);
+    let YY = dob.slice(2, 4);
+    console.log("YY is ", YY);
+    let MM = dob.slice(5, 7);
+    let DD = dob.slice(8, 10);
+    console.log("MM is ", MM);
+    console.log("DD is ", DD);
+  
+    CC = parseInt(CC);
+    YY = parseInt(YY);
+    MM = parseInt(MM);
+    DD = parseInt(DD);
+  
+    let sol = parseInt(section1(CC) + section2(YY) + section3(MM) + DD);
+    console.log("initial", sol);
+    sol = sol % 7;
+  
+    console.log(sol);
+  
+    let name = "";
+  
+    if (sol === 0) {
+      if (genderInput === "male") {
+        name = "Kwasi";
+      } else {
+        name = "Akosua";
+      }
+    } else if (sol === 1) {
+      if (genderInput === "male") {
+        name = "Kwadwo";
+      } else {
+        name = "Adwoa";
+      }
+    } else if (sol === 2) {
+      if (genderInput === "male") {
+        name = "Kwabena";
+      } else {
+        name = "Abenaa";
+      }
+    } else if (sol === 3) {
+      if (genderInput === "male") {
+        name = "Kwaku";
+      } else {
+        name = "Akua";
+      }
+    } else if (sol === 4) {
+      if (genderInput === "male") {
+        name = "Yaw";
+      } else {
+        name = "Yaa";
+      }
+    } else if (sol === 5) {
+      if (genderInput === "male") {
+        name = "Kofi";
+      } else {
+        name = "Afua";
+      }
+    } else if (sol === 6) {
+      if (genderInput === "male") {
+        name = "Kwaku";
+      } else {
+        name = "Akua";
+      }
+    } else {
+      if (genderInput === "male") {
+        name = "Kwame";
+      } else {
+        name = "Ama";
+      }
+    }
+  
+    akan_name_el.innerHTML = name;
+  }
+    
+      
